be445905aa540ce26c80ebf6a71b5fd3b948fd6c
Giovanni <giovannipor23@gmail.com>
2021-04-06 19:14:54 +0200

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 var app = new Vue ({
-    el: '#root'
+    el: '#root',
+    data: {
+        query: "",
+        movies: [],
+    },
+    
+    methods: {
+            printMovies() {
+                const self = this;
+                axios
+                .get('https://api.themoviedb.org/3/search/movie', { params: {
+                    api_key: '5417de2452f12562ccb44aa243d20d03',
+                    query: self.query,
+                    language: 'it-IT'
+                    }
+                })
+                .then((response) => {
+                    self.movies = response.data.results;
+                    console.log(self.movies);
+                    self.query = "";
+                })
+            }
+    }
 })
 
