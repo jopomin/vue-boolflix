42cd9dd1e208663c4f17334bcdd93043fe6ecb7f
Giovanni <giovannipor23@gmail.com>
2021-04-07 14:28:22 +0200

    Language Flags

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ var app = new Vue ({
     data: {
         query: "",
         movies: [],
+        flags: [
+            'fr.png',
+            'es.png',
+            'pt.png',
+            'en.png',
+            'it.png',
+            'de.png',
+            'ru.png',
+            'ko.png',
+            'ja.png',
+            'zh.png',
+            'ar.png',
+            'tr.png',
+            'cs.png',
+            'nl.png',
+            'sv.png',
+            'hr.png',
+            'sr.png',
+            'no.png',
+            'da.png',
+            'fi.png',
+            'el.png'
+        ]
     },
     
     methods: {
@@ -20,6 +43,40 @@ var app = new Vue ({
                     console.log(self.movies);
                     self.query = "";
                 })
+            },
+
+            getTitleRed(i) {
+                if (this.movies[i].title.length > 25) {
+                    return this.movies[i].title.slice(0,25) + '...';
+                } else {
+                    return this.movies[i].title
+                }
+            },
+
+            getOrTitleRed(i) {
+                if (this.movies[i].original_title.length > 35) {
+                    return this.movies[i].original_title.slice(0,35) + '...';
+                } else {
+                    return this.movies[i].original_title
+                }
+            },
+
+            getRating(i) {
+                return parseInt(this.movies[i].vote_average / 2);
+            },
+
+            getFlag(i) {
+                if (this.flags.includes(this.movies[i].original_language+'.png')) {
+                    const indexFlag = this.flags.indexOf(this.movies[i].original_language+'.png');
+                    console.log(indexFlag);
+                    return this.flags[indexFlag];
+                } else {
+                    return this.movies[i].original_language
+                }
+            },
+
+            getPoster(i) {
+                return this.movies[i].poster_path
             }
     }
 })
