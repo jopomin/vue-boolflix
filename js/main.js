e538ccf8da4d5c10ea01d428674f8ea13bf9b4da
Giovanni <giovannipor23@gmail.com>
2021-04-11 18:18:05 +0200

    Milestone 5: Aggiunta cast

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var app = new Vue ({
         series: [],
         media: [],
         ids: [],
+        cast: [],
         flags: [
             'fr.png',
             'es.png',
@@ -32,6 +33,10 @@ var app = new Vue ({
         card: {
             visible: false,
             index: false,
+        },
+        actor: {
+            visible: false,
+            index: false
         }
     },
     
@@ -46,6 +51,7 @@ var app = new Vue ({
                     }
                 })
                 .then((response) => {
+                    console.log(response);
                     self.movies = response.data.results;
                 });
 
@@ -58,18 +64,28 @@ var app = new Vue ({
                 })
                 .then((response) => {
                     self.series = response.data.results;
-                    self.media = [...self.movies,...self.series];
-                    self.movies = "";
-                    self.series = "";
-                    self.media.sort((a,b) => (a.popularity > b.popularity) ? -1 : (a.popularity < b.popularity) ? 1 : 0);
-                    console.log(self.media);
-                    self.media.forEach((med) => {
-                        if (!self.ids.includes(med.id)) {
-                            self.ids.push(med.id)
-                        }
-                    });
+                });
+                
+                self.media = [...self.movies,...self.series];
+                self.media.sort((a,b) => (a.popularity > b.popularity) ? -1 : (a.popularity < b.popularity) ? 1 : 0);
+                console.log(self.media);
+/*                 self.media.forEach((med) => {
+                    if (!self.ids.includes(med.id)) {
+                        self.ids.push(med.id)
+                    }
                     console.log(self.ids);
                 });
+
+                for (let i = 0; i < self.ids.length; i++)
+                axios
+                    .get('https://api.themoviedb.org/3/movie/self.ids[i]/credits', { params: {
+                        api_key: '5417de2452f12562ccb44aa243d20d03',
+                        language: 'it-IT'
+                        }
+                    })
+                    .then((response) => {
+                        console.log(response.data.results);
+                    }); */
             },
 
             getRating(rate) {
@@ -98,8 +114,8 @@ var app = new Vue ({
             },
 
             getOverRed(overview) {
-                if (overview.length > 320) {
-                    return overview.slice(0,320) + '...';
+                if (overview.length > 250) {
+                    return overview.slice(0,250) + '...';
                 } else {
                     return overview
                 }
@@ -118,6 +134,24 @@ var app = new Vue ({
                 this.card.visible = false;
                 this.card.index = false;
             },
+
+            getCast(id, i) {
+                const url = 'https://api.themoviedb.org/3/movie/'+id+'/credits?api_key=5417de2452f12562ccb44aa243d20d03&language=it-IT';
+                axios
+                    .get(url)
+                    .then((response) => {
+                        this.cast = [];
+                        for (let i = 0; i < 5; i++) {
+                            this.cast.push(response.data.cast[i].name);
+                        }
+                    });
+                if (this.actor.visible !== false && this.actor.index !== i) {
+                    this.actor.visible = false;
+                    this.actor.index = false;
+                }
+                this.actor.visible = !this.actor.visible;
+                this.actor.index = i;
+            }
 
     }
 })
